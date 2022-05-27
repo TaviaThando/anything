68f2c4aaf4b6c10be736f6e0a6224eef4556b254
octmoyo021 <octmoyo021@student.wethinkcode.co.za>
2022-05-27 14:29:54 +0200

    added function doing_something

--- something.py
+++ something.py
@@ -1,1 +1,12 @@
 print("I am doing something")
+
+def doing_something():
+    someone = input("Do you want me to do something? ")
+
+    if someone == yes:
+	print("Thats a shame, I'm lazy")
+    else:
+	print("That's what I wanted to hear!")
+
+
+doing_something()
